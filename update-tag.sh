391b68f2f9680a539137a9b346e5d1b1995276d6
Himanshu Singh Chauhan <90695427+HimanshuChauhan007@users.noreply.github.com>
2022-02-10 16:34:41 +0530

    Update and rename update_tag.sh to update-tag.sh

---
+++ update-tag.sh
@@ -0,0 +1,25 @@
+token=`curl https://accounts.zoho.com/oauth/v2/token -X POST -d "client_id=XXXXXXXXXXXX" -d "client_secret=XXXXXXXXXXXX" -d "refresh_token=XXXXXXXXXXXX" -d "grant_type=refresh_token" | jq '.access_token' | tr '"' ' '`
+ 
+echo $token 
+
+curl https://www.site24x7.com/api/monitors/XXXXXXXXXXXX \
+    -X PUT \
+    -H "Content-Type: application/json;charset=UTF-8" \
+    -H "Accept: application/json; version=2.1" \
+    -H "Authorization: Zoho-oauthtoken $token" \
+    -d '{
+        "display_name": "Name",
+        "type": "URL",
+        "website": "Type your URL",
+        "check_frequency": "1",
+        "timeout": 10,
+        "http_method": "P",
+        "location_profile_id": "XXXXXXXXXXXX",
+        "notification_profile_id": "XXXXXXXXXXXX",
+        "threshold_profile_id": "XXXXXXXXXXXX",
+        "user_group_ids": ["XXXXXXXXXXXX"],
+        "tag_ids":[
+            "XXXXXXXXXXXX",
+            "XXXXXXXXXXXX",
+          ]
+    }'
